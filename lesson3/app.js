0c06d1ac3c7f0420e9dd55e5287af81fe7728596
yjshi <380774061@qq.com>
2016-07-07 13:15:17 +0800

    submit lesson3

---
+++ lesson3/app.js
@@ -0,0 +1,33 @@
+var express = require('express'),
+    superagent = require('superagent'),
+    cheerio = require('cheerio');
+
+var app = express();
+
+app.get('/', function (req, res, next) {
+    superagent.get('https://cnodejs.org/')
+        .end(function (err, sres) {
+            if (err) {
+                return next(err);
+            }
+
+            var $ = cheerio.load(sres.text);
+            var items = [];
+
+            $('.cell').each(function (idx, element) {
+                var $element = $(element);
+                items.push({
+                    title: $element.find('.topic_title').attr('title'),
+                    href: $element.find('.topic_title').attr('href'),
+                    author: $element.find('img').attr('title')
+                });
+            });
+
+            res.send(items);
+        });
+});
+
+
+app.listen(3000, function () {
+    console.log('app is listening at port 3000');
+});
